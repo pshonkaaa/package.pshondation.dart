1571325368b347a957fdcb325be452ceab726b48
non.auth0 <truecollaboration0@gmail.com>
2022-05-05 22:13:49 +0200

    0.0.3: beta

--- lib/src/core/features/Logger/ELogLevel.dart
+++ lib/src/core/features/Logger/ELogLevel.dart
@@ -1,14 +1,28 @@
+import 'package:true_core/src/core/features/Logger/EAnsiColor.dart';
+
 class ELogLevel {
   final String label;
   final int id;
-  const ELogLevel(this.id, this.label);
-  
+  final EAnsiColor color;
+  const ELogLevel(
+    this.id,
+    this.label,
+    this.color,
+  );
 
-  static const ELogLevel INFO    = const ELogLevel(0, "INFO");
-  static const ELogLevel DEBUG   = const ELogLevel(1, "DEBUG");
-  static const ELogLevel WARN    = const ELogLevel(2, "WARN");
-  static const ELogLevel ERROR   = const ELogLevel(3, "ERROR");
-  static const ELogLevel ASSERT  = const ELogLevel(4, "ASSERT");
+  static const values = [
+    INFO,
+    DEBUG,
+    WARN,
+    ERROR,
+    ASSERT,
+  ];
+  
+  static const ELogLevel INFO    = const ELogLevel(0, "INFO", EAnsiColor.BRIGHT_BLUE);
+  static const ELogLevel DEBUG   = const ELogLevel(1, "DEBUG", EAnsiColor.BRIGHT_MAGENTA);
+  static const ELogLevel WARN    = const ELogLevel(2, "WARN", EAnsiColor.BRIGHT_YELLOW);
+  static const ELogLevel ERROR   = const ELogLevel(3, "ERROR", EAnsiColor.BRIGHT_RED);
+  static const ELogLevel ASSERT  = const ELogLevel(4, "ASSERT", EAnsiColor.GREEN);
 
   @override
   bool operator ==(other) {
