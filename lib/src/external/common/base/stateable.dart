731982e8c2c0c110f5819c16bd869238595aba71
TrueCollaboration <pshonka.dev@gmail.com>
2023-11-14 22:15:12 +0100

    Update stateable.dart

--- lib/src/external/common/base/stateable.dart
+++ lib/src/external/common/base/stateable.dart
@@ -1,24 +1,42 @@
+import 'dart:async';
+
 import 'package:meta/meta.dart';
 import 'package:true_core/library.dart';
 
 abstract class BaseStateable implements IStateable {
   @override
-  bool initialized = false;
+  bool get initialized => _initialized;
 
   @override
-  bool disposed = false;
+  bool get disposed => _disposed;
+
+  bool _initialized = false;
+  
+  bool _disposed = false;
 
   @override
   @mustCallSuper
   void initState() {
-    initialized = true;
-    disposed = false;
+    _initialized = true;
+    _disposed = false;
   }
 
   @override
   @mustCallSuper
   void dispose() {
-    initialized = false;
-    disposed = true;
+    _initialized = false;
+    _disposed = true;
+  }
+}
+
+abstract class BaseAsyncStateable extends BaseStateable implements IAsyncStateable {
+  @override
+  FutureOr<void> initState() {
+    super.initState();
+  }
+  
+  @override
+  FutureOr<void> dispose() {
+    super.dispose();
   }
 }
